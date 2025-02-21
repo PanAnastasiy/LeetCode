c2cdfcd6e1cf2e845b790b6892a1dca271fcab8a
PanAnastasiy <stanislav.panfilenko@gmail.com>
2025-02-21 18:14:39 +0300

    21-02-2025-task-3

--- 169 Majority Element.py
+++ 169 Majority Element.py
@@ -1,7 +1,16 @@
 class Solution(object):
+
+    '''
+    def majorityElement(self, nums: List[int]) -> int:
+     return max(list(filter(lambda x: nums.count(x) > len(nums) / 2, nums)))
+    '''
+
+
     @staticmethod
     def majorityElement(nums):
-        return max(list(filter(lambda x: nums.count(x) > len(nums) / 2, nums)))
+        dct = {num: nums.count(num) for num in set(nums)}
+        return max(dct, key=dct.get)
 
 
-print(Solution.majorityElement([2,2,1,1,1,2,2]))
+print(Solution.majorityElement([2, 2, 1, 1, 1, 2, 2]))
+print(Solution.majorityElement([2, 2, 1, 1, 1, 2, 2, 2, 2]))
