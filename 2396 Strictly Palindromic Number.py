ba1ccd6819adafaa4859b852477d6b3f9da5b624
PanAnastasiy <stanislav.panfilenko@gmail.com>
2025-05-29 20:27:11 +0300

    29-05-2025-task-1

---
+++ 2396 Strictly Palindromic Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+
+
+print(Solution().isStrictlyPalindromic(9))
+print(Solution().isStrictlyPalindromic(4))
