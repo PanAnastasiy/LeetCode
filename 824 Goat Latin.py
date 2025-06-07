5fef7d24874dc44ae1cba42eafaeb9eaf9ff389c
PanAnastasiy <stanislav.panfilenko@gmail.com>
2025-06-07 18:31:22 +0300

    07-06-2025-task-1

---
+++ 824 Goat Latin.py
@@ -0,0 +1,11 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        words = sentence.split()
+        for i in range(len(words)):
+            if words[i][0].lower() in 'aeiou':
+                words[i] += 'ma'
+            else:
+                words[i] += words[i][0] + 'ma'
+                words[i] = words[i][1:]
+            words[i] += 'a' * (i + 1)
+        return ' '.join(words)
