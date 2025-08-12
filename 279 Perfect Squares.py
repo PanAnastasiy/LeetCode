a8da6dcc950866a25585d93d87e9ed8ae055db0b
PanAnastasiy <stanislav.panfilenko@gmail.com>
2025-08-12 14:58:43 +0300

    12-08-2025-task-1

--- 279 Perfect Squares.py
+++ 279 Perfect Squares.py
@@ -1,13 +1,14 @@
 
 class Solution(object):
 
-    @staticmethod
-    def numSquares(n):
-        prefect_squares = [i**2 for i in range(1, int(n**0.5 + 1))]
-        print(prefect_squares)
-        for number in range(n):
-            pass
-        return 1
+    def numSquares(self, n):
+        dp = [0] + [float('inf')] * n
+        for i in range(1, n + 1):
+            for j in range(1, int(n**0.5) + 1):
+                dp[i] = min(dp[i], dp[i - j*j] + 1)
+        return dp[-1]
 
 
-Solution.numSquares()
+print(Solution().numSquares(36))
+print(Solution().numSquares(12))
+print(Solution().numSquares(13))
