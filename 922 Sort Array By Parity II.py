1ee43e16ee6845400b625193cd1fe6867e3edf0c
PanAnastasiy <stanislav.panfilenko@gmail.com>
2025-08-15 12:05:13 +0300

    15-08-2025-task-2

--- 922 Sort Array By Parity II.py
+++ 922 Sort Array By Parity II.py
@@ -1,12 +1,16 @@
 class Solution:
     def sortArrayByParityII(self, nums: list[int]) -> list[int]:
-        first, second = 1, len(nums) // 2
-        while second <= len(nums) - 1:
-            nums[first], nums[second] = nums[second], nums[first]
+        nums.sort(key=lambda x: x % 2 == 0)
+        first = 0
+        n = len(nums)
+        while first < n // 2:
+            nums[first], nums[n - first - 1] = nums[n - first - 1], nums[first]
             first += 2
-            second += 2
         return nums
 
 
 print(Solution().sortArrayByParityII([4, 2, 5, 7]))
 print(Solution().sortArrayByParityII([2, 3]))
+print(Solution().sortArrayByParityII([3, 7, 2, 4]))
+print(Solution().sortArrayByParityII([4, 1, 2, 1]))
+
