d09df69725e25972ccf2c88ada3627d039bc987b
PanAnastasiy <stanislav.panfilenko@gmail.com>
2025-08-24 22:47:55 +0300

    24-08-2025-task-1

--- 2540 Minimum Common Value.py
+++ 2540 Minimum Common Value.py
@@ -1,13 +1,4 @@
 class Solution:
     def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
-
-        def check(arr1, arr2):
-            dct_1 = {a: arr1.count(a) for a in set(arr1)}
-            dct_2 = {a: arr2.count(a) for a in set(arr2)}
-
-            for key in dct_1:
-                if dct_2[key]:
-                    return key
-            return -1
-
-        return check(nums1, nums2) if len(nums1) > len(nums2) else check(nums2, nums1)
+        i = set(nums1) & set(nums2)
+        return min(i) if i else -1
