956cf265361bbb7c04bd75a817b525ab304bfe73
Your Name <youremail@example.com>
2026-02-21 15:35:02 +0300

    21-02-2026-task-6

--- 3507 Minimum Pair Removal to Sort Array I.py
+++ 3507 Minimum Pair Removal to Sort Array I.py
@@ -2,8 +2,16 @@ class Solution:
     def minimumPairRemoval(self, nums: list[int]) -> int:
         answer = 0
         while nums != sorted(nums):
-            minimum = sum(nums[:2])
-            for i in range(0, len(nums) - 1, 2):
-                if sum(nums[i: i + 2]) < minimum:
-                    minimum = sum(nums[i: i + 2])
+            minimum = nums[0] + nums[1]
+            index = 0
+            for i in range(len(nums) - 1):
+                if nums[i] + nums[i + 1] < minimum:
+                    minimum = nums[i] + nums[i + 1]
+                    index = i
+            nums[index] = minimum
+            nums.pop(index + 1)
+            answer += 1
+        return answer
             
+
+print(Solution().minimumPairRemoval([5,2,3,1]))
